f21b3df165018f44fd6f9f0b72b724b30f9d6f0f
atomiyama <tomisuker16@gmail.com>
2026-03-11 18:50:32 +0900

    fix: point DMG download URL to homebrew-tap releases

--- Casks/timedog.rb
+++ Casks/timedog.rb
@@ -2,7 +2,7 @@ cask "timedog" do
   version "1.3.0"
   sha256 "05c887f29912121cec2e2a142cac93d752c3d5579ca73ea29e172bfaa84843c0"
 
-  url "https://github.com/viteflowsystem/timedog/releases/download/v#{version}/Timedog-#{version}-universal.dmg"
+  url "https://github.com/viteflowsystem/homebrew-tap/releases/download/timedog_v#{version}/Timedog-#{version}-universal.dmg"
   name "Timedog"
   desc "Freelance timecard app with Google Sheets integration"
   homepage "https://github.com/viteflowsystem/timedog"
