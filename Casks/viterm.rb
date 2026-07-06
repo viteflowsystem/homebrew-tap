613bfd5d546c7fe5ed000c242e070967b430aa8e
a-tommyyy <tomisuker16@gmail.com>
2026-07-06 20:33:53 +0900

    Add viterm cask v0.1.0

---
+++ Casks/viterm.rb
@@ -0,0 +1,18 @@
+cask "viterm" do
+  version "0.1.0"
+  sha256 "bfd6968a376e10cc50e82e8224c8767e7711d5a6cdebd0465a733526b7977797"
+
+  url "https://github.com/viteflowsystem/homebrew-tap/releases/download/viterm_v#{version}/viterm-#{version}.dmg"
+  name "viterm"
+  desc "Native macOS terminal for running AI coding agents in parallel"
+  homepage "https://github.com/viteflowsystem/viterm"
+
+  depends_on macos: ">= :sequoia"
+
+  app "viterm.app"
+
+  zap trash: [
+    "~/.config/viterm",
+    "~/Library/Application Support/viterm"
+  ]
+end
