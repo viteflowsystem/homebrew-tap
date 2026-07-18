42349ed95ef8fa57aa93290c0a1f1513ba1c0431
a-tommyyy <tomisuker16@gmail.com>
2026-07-18 20:43:46 +0900

    viterm 0.5.0

--- Casks/viterm.rb
+++ Casks/viterm.rb
@@ -1,6 +1,6 @@
 cask "viterm" do
-  version "0.4.0"
-  sha256 "53969845b144fb4d7104837a377f55d5c36474b9c3b0b206cb8a51fc3af9be77"
+  version "0.5.0"
+  sha256 "58546a061696b87ca6acb984c7a394cfce9d0cbf300494bd89f831348e59035e"
 
   url "https://github.com/viteflowsystem/homebrew-tap/releases/download/viterm_v#{version}/viterm-#{version}.dmg"
   name "viterm"
